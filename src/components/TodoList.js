016f5e376c7a7139ec88aa5a27d2acf440dd2cab
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-05-01 17:42:40 +0530

    done 216 with new components

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TodoList = () => {
+    return (
+        <div>
+            <ul>
+                <li>Pay Bill</li>
+                <li>Do your Homework</li>
+                <li>Feed the Dog</li>
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
